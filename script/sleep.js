abd02af929bd37bc7f1c1733c8bc0d5d3801d73d
Dr. Safi <sfmissive@gmail.com>
2019-01-29 23:01:21 +0200

    Export script as function to support calling the js api from cli

--- script/sleep.js
+++ script/sleep.js
@@ -1,5 +1,14 @@
 import path from 'path'
-// (function endlessProcess() { process.nextTick(endlessProcess) })() // Readable solution but it utilizes all available CPU. https://stackoverflow.com/questions/39082527/how-to-prevent-the-nodejs-event-loop-from-exiting
-console.log(`Executing interval in ${__filename}. NodeJS version: ${JSON.stringify(process.versions)}`)
-setInterval(function(){ console.info("Sleeping..."); }, 1000);
-setTimeout(() => { console.log('setTimeout command ended. The process will exit now.') }, 100000);
+
+module.exports = {
+    setInterval: function ({ interval = 1000 } = {}) {
+        // (function endlessProcess() { process.nextTick(endlessProcess) })() // Readable solution but it utilizes all available CPU. https://stackoverflow.com/questions/39082527/how-to-prevent-the-nodejs-event-loop-from-exiting
+        console.log(`Executing interval in ${__filename}. NodeJS version: ${JSON.stringify(process.versions)}`)
+        setInterval(function(){ console.info("Sleeping..."); }, interval);
+    },
+    setTimeout: function ({ timeout = 10000 } = {}) {
+        setTimeout(() => { console.log('setTimeout command ended. The process will exit now.') }, timeout );
+    }
+}
+
+    
