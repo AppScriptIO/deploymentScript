b6c347ce0395a4e3181230360e6c30619b43dfec
Dr. Safi <sfmissive@gmail.com>
2019-02-10 23:29:12 +0200

    📝 Add todo list for the github urls version bump tool.

---
+++ script/versionGithub/script.js
@@ -0,0 +1,12 @@
+/**
+ * TODO: 
+ * • Read package.json for github urls. 
+ * • Extract URL and any commit or tag value.
+ * • Hit github API to check for newer tags or newer releases. 
+ * • Bump version - update package.json in case a newer version is found.
+ */
+export function checkLatestVersionOnGithub() {
+    console.log('Bump version for github dependency.')
+
+}
+
