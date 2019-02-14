20f4f98d8ef0091a3afedf66b0a19b8fd842d3ea
Dr. Safi <sfmissive@gmail.com>
2019-02-14 11:03:42 +0200

    ✒ Do not use cache in `apollo-client` during graphql query.

--- script/versionGithub/utility/createGraphqlClient.js
+++ script/versionGithub/utility/createGraphqlClient.js
@@ -3,15 +3,28 @@ import { HttpLink } from 'apollo-link-http'
 import { ApolloClient } from 'apollo-client'
 import nodeFetch from 'node-fetch'
 
+// reference: https://www.apollographql.com/docs/react/api/apollo-client.html#apollo-client 
+const defaultOptions = {
+    watchQuery: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'ignore',
+    },
+    query: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all',
+    },
+}
+
 export function createGraphqlClient({ endpoint, token }) { 
     return new ApolloClient({
-        link: new HttpLink({ 
+        link: new HttpLink({
             fetch: nodeFetch, 
             uri: endpoint,
             headers: {
                 Authorization: `bearer ${token}`
             }
         }),
-        cache: new InMemoryCache()
+        cache: new InMemoryCache(), 
+        defaultOptions
     })
 }
