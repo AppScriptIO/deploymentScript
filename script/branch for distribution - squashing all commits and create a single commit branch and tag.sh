78ad4aadfce2d162657fa0fa040f326131f4c48c
Safi <sfmissive@gmail.com>
2019-05-09 09:22:39 +0300

    🐞 Do not delete distribution folder after release.

--- script/branch for distribution - squashing all commits and create a single commit branch and tag.sh
+++ script/branch for distribution - squashing all commits and create a single commit branch and tag.sh
@@ -10,6 +10,6 @@ find . \
     -path ./.git -prune -o \
     -path ./node_modules -prune -o \
     -exec rm -rf {} \; 2> /dev/null
-mv ./distribution/* . && rm -r distribution
+cp -r ./distribution/* .
 git add -A && git commit -a -m 'build' && git tag 1.0.4; 
 git checkout master
