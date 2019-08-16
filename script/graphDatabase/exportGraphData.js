8ea9b1854c43cc9de4a3e3941ac2f00f7239726d
Safi <sfmissive@gmail.com>
2019-08-16 17:17:51 +0300

    ➕ Add script for fixing json graph data, where it rewrites a json grpah data according to the desired modifications.

--- script/graphDatabase/exportGraphData.js
+++ script/graphDatabase/exportGraphData.js
@@ -7,6 +7,8 @@ import { Entity } from '@dependency/entity'
 import { Database as DatabaseModule } from '@dependency/graphTraversal'
 const { Database } = DatabaseModule
 import { boltCypherModelAdapterFunction } from '@dependency/graphTraversal/source/implementationPlugin/databaseModelAdapter/boltCypherModelAdapter.js'
+import { file } from 'babel-types'
+const uuidv4 = require('uuid/v4')
 
 let concreteDatabaseBehavior = new Database.clientInterface({
   implementationList: { boltCypherModelAdapter: boltCypherModelAdapterFunction() },
@@ -34,6 +36,36 @@ export async function exportGraphData({ api, targetPath = './test/asset/', fileN
   await filesystem.writeFile(path.join(exportPath, fileName), graphData, { encoding: 'utf8', flag: 'w' /*tructace file if exists and create a new one*/ })
   console.log(`• Created json file - ${path.join(exportPath, fileName)}`)
   concereteDatabase.driverInstance.close()
+}
+
+/** This function rewrites the json file - any modifications should be added in the function.
+ * `yarn run scriptManager shouldCompileScript=true graphDatabase/exportGraphData ".fixJSONData({ })"`
+ */
+export async function fixJSONData({ api, targetPath = './resource/', fileName = 'fixed.exported.json' } = {}) {
+  const targetProjectRootPath = api.project.configuration.configuration.directory.root
+  let graphData = require(path.join(targetProjectRootPath, targetPath, 'taskSequence.graphData.json'))
+
+  // modify data
+  graphData.node = graphData.node.map(item => {
+    // add key to nodes without key
+    if (!item.properties.key) {
+      console.log(`• Fixing node without key - ` + JSON.stringify(item))
+      item.properties.key = uuidv4()
+    }
+    return item
+  })
+  graphData.edge = graphData.edge.map(item => {
+    // add key to connections without key
+    if (!item.properties.key) {
+      console.log(`• Fixing edge without key - ` + JSON.stringify(item))
+      item.properties.key = uuidv4()
+    }
+    return item
+  })
+
+  const exportPath = path.normalize(path.join(targetProjectRootPath, targetPath))
+  await filesystem.writeFile(path.join(exportPath, fileName), graphData |> JSON.stringify, { encoding: 'utf8', flag: 'w' /*tructace file if exists and create a new one*/ })
+  console.log(`• Created json file - ${path.join(exportPath, fileName)}`)
 }
 
 async function clearDatabase() {
