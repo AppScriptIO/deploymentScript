7f12dadb5f2bd13e9276d3cd8aad1f8a9d130657
Safi <nassarsafi@gmail.com>
2019-10-21 01:08:21 +0300

    ✒ Add script for jshint installation.

---
+++ script/provisionOS/installJshint.js
@@ -0,0 +1,9 @@
+const childProcess = require('child_process')
+const childProcessOption = { cwd: __dirname, shell: true, stdio: [0, 1, 2] }
+const { sync: binaryExist } = require('command-exists')
+
+// Installed globally for usage with VSCode extension
+export function npmInstall() {
+  if (binaryExist('jshint')) console.log('✔ jshint (npm package) is installed.')
+  else childProcess.execSync('npm install -g jshint', childProcessOption)
+}
