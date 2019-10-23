1a2640a66ee93b1bc855b243388612010fe06ece
Safi <nassarsafi@gmail.com>
2019-10-23 11:36:27 +0300

    📄 Add commentary.

--- script/graphDatabase/memgraphContainer.js
+++ script/graphDatabase/memgraphContainer.js
@@ -5,6 +5,7 @@ const childProcessOption = { cwd: __dirname, shell: true, stdio: [0, 1, 2] }
 // `-v mg_lib:/var/lib/memgraph -v mg_log:/var/log/memgraph -v mg_etc:/etc/memgraph`
 export function runDockerContainer() {
   console.log(`• Running container: memgraph on port 7687`)
+  // TODO: This is a quick automatic starting of dependency container. Provide a better way to handle container dependencies.
   try {
     childProcess.execSync('docker run -p 7687:7687 -d memgraph', childProcessOption)
   } catch (error) {
