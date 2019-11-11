69e3592d4d0b8cc56bb13085842008d32ed0e40b
Safi <nassarsafi@gmail.com>
2019-11-11 14:08:24 +0200

    ⚗📦 Move all dependencies to the root package.json. ✒ Deal with nodegit and it's external dependency installation.

--- configuration/project.config.js
+++ configuration/project.config.js
@@ -16,9 +16,18 @@ const ownConfig = {
     get script() {
       return path.join(ownConfig.directory.root, './script')
     },
+    get packageManager() {
+      return path.join(ownConfig.directory.root, './packageManager')
+    },
   },
   get script() {
-    return [...script, ...[{ type: 'directory', path: ownConfig.directory.script }, { type: 'directory', path: ownConfig.directory.source }]]
+    return [
+      ...script,
+      ...[
+        { type: 'directory', path: ownConfig.directory.script },
+        { type: 'directory', path: ownConfig.directory.source },
+      ],
+    ]
   },
   entrypoint: {
     programmaticAPI: './script.js',
@@ -32,7 +41,11 @@ const ownConfig = {
   },
   build: {
     get compile() {
-      return [path.relative(ownConfig.directory.root, ownConfig.directory.source), path.relative(ownConfig.directory.root, ownConfig.directory.script)]
+      return [
+        path.relative(ownConfig.directory.root, ownConfig.directory.source),
+        path.relative(ownConfig.directory.root, ownConfig.directory.script),
+        path.relative(ownConfig.directory.root, ownConfig.directory.packageManager),
+      ]
     },
     repositoryURL: 'https://github.com/AppScriptIO/deploymentScript',
   },
