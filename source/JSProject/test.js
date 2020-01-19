fa0692d8960ef8ef89149bffc24d7b91a3ed6cdc
Safi <nassarsafi@gmail.com>
2020-01-19 23:45:56 +0200

    ⚗ Refactor test module execution - Simplify usage of test runner, through external file lookup for js and tests and external control of watcher & reload functions.

--- source/JSProject/test.js
+++ source/JSProject/test.js
@@ -1,7 +1,32 @@
-const { runTest } = require('@dependency/javascriptTestRunner')
+const { runTest, resolveAndLookupFile } = require('@dependency/javascriptTestRunner')
+import { watchFile } from '@dependency/nodejsLiveReload'
 
-module.exports = function(...args) {
-  const { api /* supplied by scriptManager */ } = args[0]
-  args[0].targetProject = api.project // adapter for working with target function interface.
-  runTest(...args)
+module.exports = async function({
+  api /* supplied by scriptManager */,
+  testPath = [], // relative or absolute paths
+  jsPath = [],
+  shouldCompileTest,
+  shouldDebugger,
+} = {}) {
+  let targetProjectRootPath = api.project.configuration.rootPath // from scriptManager api.
+
+  let testFileArray = resolveAndLookupFile({ pathArray: [...testPath], basePath: targetProjectRootPath, fileExtension: ['.test.js'] })
+  let jsFileArray = resolveAndLookupFile({ pathArray: [...jsPath, ...testFileArray, targetProjectRootPath], basePath: targetProjectRootPath, fileExtension: ['.js', '.ts'] })
+
+  let { restart: restartTest } = await runTest({
+    targetProject: api.project /**adapter for working with target function interface*/,
+    shouldCompileTest,
+    shouldDebugger,
+    testFileArray,
+    jsFileArray,
+  })
+
+  await watchFile({
+    // to be run after file notification
+    triggerCallback: () => restartTest(),
+    // TODO: make sure explicitly adding `./node_modules/` into the this array, will prevent it from being ignored.
+    fileArray: jsFileArray,
+    ignoreNodeModules: true,
+    logMessage: true,
+  })
 }
