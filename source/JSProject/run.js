13e2f17fdd963246ea9c099b2539bc7b5a743361
Safi <nassarsafi@gmail.com>
2020-01-20 19:39:05 +0200

    ✒ Add clear graph function, to allow reloading server with fresh graph database. ⚗ Move ManageSubprocess class into own repository.

--- source/JSProject/run.js
+++ source/JSProject/run.js
@@ -1,10 +1,24 @@
 import path from 'path'
 import assert from 'assert'
 import filesystem from 'fs'
-import EventEmitter from 'events'
-import childProcess from 'child_process'
-import { watchFile, browserLivereload } from '@dependency/nodejsLiveReload'
+import { watchFile, browserLivereload, ManageSubprocess } from '@dependency/nodejsLiveReload'
 const { resolveAndLookupFile } = require('@dependency/resolveAndLookupPath')
+const boltProtocolDriver = require('neo4j-driver').v1
+import { memgraphContainer } from '@dependency/deploymentProvisioning'
+
+async function clearGraphData() {
+  console.groupCollapsed('• Run prerequisite containers:')
+  memgraphContainer.runDockerContainer() // temporary solution
+  console.groupEnd()
+  // Delete all nodes in the in-memory database
+  console.log('• Cleared graph database.')
+  const url = { protocol: 'bolt', hostname: 'localhost', port: 7687 },
+    authentication = { username: 'neo4j', password: 'test' }
+  const graphDBDriver = boltProtocolDriver.driver(`${url.protocol}://${url.hostname}:${url.port}`, boltProtocolDriver.auth.basic(authentication.username, authentication.password))
+  let session = await graphDBDriver.session()
+  let result = await session.run(`match (n) detach delete n`)
+  session.close()
+}
 
 function setInterval({ interval = 1000 } = {}) {
   // (function endlessProcess() { process.nextTick(endlessProcess) })() // Readable solution but it utilizes all available CPU. https://stackoverflow.com/questions/39082527/how-to-prevent-the-nodejs-event-loop-from-exiting
@@ -12,46 +26,6 @@ function setInterval({ interval = 1000 } = {}) {
   setInterval(() => console.info('Sleeping...'), interval)
 }
 const setTimeout = ({ timeout = 10000 } = {}) => setTimeout(() => console.log('setTimeout command ended. The process will exit now.'), timeout)
-
-class ManageSubprocess extends EventEmitter {
-  subprocess // child subprocess
-  cliAdapterPath // the path to the cli entrypoint file, that will receive arguments from the child process fork function and pass it to the programmatic module api.
-  argumentList // cached arguments to be used for running subprocesses
-
-  constructor({ cliAdapterPath }) {
-    super()
-    this.cliAdapterPath = cliAdapterPath
-    // clean up if an error goes unhandled.
-    process.on('exit', () => this.subprocess && this.subprocess.kill())
-    process.on('SIGINT', () => console.log('Caught interrupt signal') && process.exit(0))
-  }
-
-  runInSubprocess() {
-    if (this.subprocess) this.subprocess.kill()
-
-    this.argumentList = arguments.length == 0 ? this.argumentList || [] : arguments
-
-    let stringifyArgs = JSON.stringify(this.argumentList) // parametrs for module to be run in subprocess.
-    // running in subprocess prevents allows to control the application and terminate it when needed.
-    this.subprocess = childProcess
-      .fork(this.cliAdapterPath, [stringifyArgs], {
-        stdio: [0, 1, 2, 'ipc'],
-        execArgv: [
-          // '--inspect-brk=1272', // inspect subprocess with random port to prevent conflicts with the main process in case it's inspect flag was turned on.
-          '--no-lazy', // for debugging purposes will load modules sequentially
-        ],
-      })
-      .on('exit', () => console.log(`subprocess ${this.subprocess.pid} exited.`))
-      .on('message', message => {
-        if (message?.status == 'ready') this.emit('ready')
-      })
-      .on('close', code => {
-        if (code === 8) console.error('Error detected, waiting for changes.')
-      })
-
-    return this.subprocess
-  }
-}
 
 /*
   Run webapp project: 
@@ -80,12 +54,35 @@ module.exports = async function({ api /* supplied by scriptManager */ } = {}) {
     rootServiceHost: targetServiceHost,
   })
 
-  // run application.
   let manageSubprocess = new ManageSubprocess({ cliAdapterPath: path.join(api.project.configuration.rootPath, 'entrypoint/cli') })
-  manageSubprocess.runInSubprocess()
   manageSubprocess.on('ready', () => reloadBrowserClient()) // reload browser after server reload
 
+  // run application:
+  await clearGraphData() // run prerequesite container and clear graph
+  manageSubprocess.runInSubprocess()
+
+  const watchFileList_serverSide = [
+    '/project/application/source/serverSide/**/*.js',
+    // '/project/application/source/serverSide/**/*.css',
+    // '/project/application/source/serverSide/**/*.html',
+    // '/project/application/source/serverSide/node_modules/appscript{/**/*.js,!/node_modules/**/*}',
+    '!/project/application/source/serverSide/node_modules{,/**/*,!/appscript/**/*}',
+    // '!/project/application/source/serverSide/node_modules/appscript/node_modules{,/**/*}',
+  ] // equals to '!/app/{node_modules,node_modules/**/*}'
+
+  await watchFile({
+    // to be run after file notification
+    triggerCallback: async () => {
+      await clearGraphData()
+      manageSubprocess.runInSubprocess()
+    },
+    fileArray: [path.join(api.project.configuration.rootPath, 'source')],
+    ignoreNodeModules: true,
+    logMessage: true,
+  })
+
   // File list to watch - Uses globs array for defining files patterns - https://github.com/isaacs/node-glob
+  // TODO: use resolveAndLookupFile function
   const watchFileList_clientSide = [
     // TODO: there is an issue when specifying multiple paths, for some reason it doesn't watch all files when separately configured, while watching all files without distinction is possible. Maybe an issue with glob strings
     // not working when separated.
@@ -99,25 +96,6 @@ module.exports = async function({ api /* supplied by scriptManager */ } = {}) {
     '!/project/application/source/clientSide/**/component.package/**/*',
     '!/project/application/source/clientSide/**/js.package.yarn/**/*',
   ] // equals to '!/project/application/source/{node_modules,node_modules/**/*}'
-
-  const watchFileList_serverSide = [
-    '/project/application/source/serverSide/**/*.js',
-    // '/project/application/source/serverSide/**/*.css',
-    // '/project/application/source/serverSide/**/*.html',
-    // '/project/application/source/serverSide/node_modules/appscript{/**/*.js,!/node_modules/**/*}',
-    '!/project/application/source/serverSide/node_modules{,/**/*,!/appscript/**/*}',
-    // '!/project/application/source/serverSide/node_modules/appscript/node_modules{,/**/*}',
-  ] // equals to '!/app/{node_modules,node_modules/**/*}'
-
-  await watchFile({
-    // to be run after file notification
-    triggerCallback: () => {
-      manageSubprocess.runInSubprocess()
-    },
-    fileArray: [path.join(api.project.configuration.rootPath, 'source')],
-    ignoreNodeModules: true,
-    logMessage: true,
-  })
 
   const clientSidePathList = clientSideProjectConfigList.map(item => item.path)
   await watchFile({
