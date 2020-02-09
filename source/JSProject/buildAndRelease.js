59ede9b884df8b2b63f4d4614de4223f2532e29d
Safi <nassarsafi@gmail.com>
2020-02-09 14:28:29 +0200

    ✒ Allow to release package using existing distribution folder.

--- source/JSProject/buildAndRelease.js
+++ source/JSProject/buildAndRelease.js
@@ -3,22 +3,22 @@ import { moduleProject as buildModuleProject, webappProject as buildWebappProjec
 import { bumpVersion } from './packageVersion'
 import * as container from './container'
 
-export async function moduleProject({ api, tagName }) {
+export async function moduleProject({ api, tagName, shouldBuildCode = true } = {}) {
   container.memgraph.runDockerContainer() // run memgraph container for usage in buildTool graphTraversal module.
   let version = await bumpVersion({ api })
   await createGithubBranchedRelease({
     api,
     tagName: tagName || version,
-    buildCallback: () => buildModuleProject({ api }),
+    buildCallback: shouldBuildCode ? () => buildModuleProject({ api }) : undefined,
   })
 }
 
-export async function webappProject({ api, tagName }) {
+export async function webappProject({ api, tagName, shouldBuildCode = true } = {}) {
   container.memgraph.runDockerContainer() // run memgraph container for usage in buildTool graphTraversal module.
   let version = await bumpVersion({ api })
   await createGithubBranchedRelease({
     api,
     tagName: version,
-    buildCallback: () => buildWebappProject({ api }),
+    buildCallback: shouldBuildCode ? () => buildWebappProject({ api }) : undefined,
   })
 }
